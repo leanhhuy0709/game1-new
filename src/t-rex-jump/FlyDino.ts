766abf3b580033510caa247d0de612fcad91c470
leanhhuy0709 <huy.leanh0709@hcmut.edu.vn>
2023-06-16 10:12:06 +0700

    FlyDino

--- src/t-rex-jump/FlyDino.ts
+++ src/t-rex-jump/FlyDino.ts
@@ -1,18 +1,27 @@
+import Sprite from '../engine/component/Sprite'
+import Collider from '../engine/component/physics/Collider'
+import Movement from '../engine/component/physics/Movement'
 import Random from '../engine/math/Random'
-import Rectangle from '../engine/shape/Rectangle'
+import { DEPTH } from '../types/depth'
 import Obstacle from './Obstacle'
 import { FLYDINO_1, FLYDINO_2, FLYDINO_3 } from './const'
 
 export default class FlyDino extends Obstacle {
-    public constructor(delay: number, x = 0) {
-        const arr = [170, 130, 100]
-        super(new Rectangle(x, arr[Random.getIntNumber(0, arr.length)], 90, 80, 'FlyDino'), [FLYDINO_1, FLYDINO_2, FLYDINO_3], delay)
+    public constructor(x: number, y: number, w: number, h: number) {
+        super(x, y, w, h)
+        this.addComponent(new Collider(this))
+        this.addComponent(new Sprite(this, [FLYDINO_1, FLYDINO_2, FLYDINO_3], DEPTH.OBJECT_MEDIUM))
+        this.addComponent(new Movement(this, 0, 0, 0))
         this.reset(x)
     }
     public reset(x: number, moveSpeed = 0.5) {
-        this.getShape().getCoord().setX(x)
+        this.setX(x)
         const arr = [170, 130, 100]
-        this.getShape().getCoord().setY(arr[Random.getIntNumber(0, arr.length)])
-        this.moveSpeed = moveSpeed
+        this.setY(arr[Random.getIntNumber(0, arr.length)])
+
+        const tmp = this.getComponent<Movement>(Movement)
+        if (tmp.length > 0) {
+            tmp[0].setVelocity(moveSpeed, -1, 0)
+        }
     }
 }
