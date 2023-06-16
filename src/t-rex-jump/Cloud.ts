e603e26cab59e350a8446cb43a68f97da22d4246
leanhhuy0709 <huy.leanh0709@hcmut.edu.vn>
2023-06-16 10:52:06 +0700

    Cloud

--- src/t-rex-jump/Cloud.ts
+++ src/t-rex-jump/Cloud.ts
@@ -1,20 +1,29 @@
+import Picture from '../engine/component/Picture'
+import Movement from '../engine/component/physics/Movement'
 import Random from '../engine/math/Random'
-import Rectangle from '../engine/shape/Rectangle'
+import { DEPTH } from '../types/depth'
 import Decor from './Decor'
 import { CLOUD } from './const'
 
 export default class Cloud extends Decor {
     public constructor(x: number, y: number, w: number, h: number) {
-        super(new Rectangle(x, y, w, h, 'Cloud'), [CLOUD], 1, Random.getIntNumber(-5, 5) / 10)
+        super(x, y, w, h)
+        //super(new Rectangle(x, y, w, h, 'Cloud'), [CLOUD], 1, Random.getIntNumber(-5, 5) / 10)
+        this.addComponent(new Picture(this, CLOUD, DEPTH.OBJECT_LOW))
+        this.addComponent(new Movement(this, 0, 0, 0))
+        this.reset(x)
     }
 
     public reset(x: number): void {
-        this.getShape().getCoord().setCoord(x, Random.getIntNumber(0, 140))
-        console.log(this.getShape().getCoord().getX())
-        const rect = this.getShape() as Rectangle
-        rect.getSize().setSize(90, 80)
-        this.moveSpeed = Random.getIntNumber(-5, 5) / 10
-        //console.log(this.getShape())
+        this.setX(x)
+        this.setY(Random.getIntNumber(0, 140))
+        this.setWidth(90)
+        this.setHeight(80)
+
+        const tmp = this.getComponent<Movement>(Movement)
+        if (tmp.length > 0) {
+            tmp[0].setVelocity(Random.getIntNumber(-5, 5) / 10, -1, 0)
+        }
     }
 
     public render(): void {
